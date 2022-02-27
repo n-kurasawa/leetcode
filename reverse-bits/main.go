f914abe9e8703b739824ad99b8cd477b2acd8b08
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-02-27 14:16:32 +0900

    reverse-bits

---
+++ reverse-bits/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func reverseBits(num uint32) uint32 {
+	var result uint32
+	for i := 0; i < 32; i++ {
+		result <<= 1
+		result |= 1 & num
+		num >>= 1
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(reverseBits(43261596))
+}
