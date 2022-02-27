7cf3a365a3419e7e63cf8b17cfa5faa883716e70
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-02-28 00:32:10 +0900

    number-of-1-bits

---
+++ number-of-1-bits/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func hammingWeight(num uint32) int {
+	result := 0
+	for i := 0; i < 32; i++ {
+		if (num&(1<<i))>>i == 1 {
+			result++
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(hammingWeight(11))
+}
