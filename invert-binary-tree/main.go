1a591281f1aec4c56a8965907cd8ac424cdff38d
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-03-17 22:18:44 +0900

    invert-binary-tree

---
+++ invert-binary-tree/main.go
@@ -0,0 +1,20 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	l := invertTree(root.Left)
+	r := invertTree(root.Right)
+	root.Left = r
+	root.Right = l
+	return root
+}
+
+func main() {}
